ce9d9c8a9a76066a695aba2e3d194195fd1e27b7
10234567Z <93607971+10234567Z@users.noreply.github.com>
2023-07-22 19:16:32 +0530

    Add all dependencies, plugins and modules inside directory

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    devtool: 'inline-source-map',
+    module:{
+        rules: [
+            {
+                test: /\.css$/,
+                use:  ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.ttf$/,
+                use: 'asset/resource',
+            },
+        ],
+    },
+    devServer:{
+        static: './dist',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Do-Thing',
+        }),
+    ],
+    output:{
+        filename: '[name].js',
+        path: path.resolve(__dirname,'dist'),
+        clean: true,
+    },
+    optimization:{
+        runtimeChunk: 'single',
+    },
+}
