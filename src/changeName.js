64f0ab16195edc78bb2b3a30d56a61eeb1e37669
10234567Z <93607971+10234567Z@users.noreply.github.com>
2023-07-29 19:37:02 +0530

    Display the newly set name in UI after submitting

--- src/changeName.js
+++ src/changeName.js
@@ -3,6 +3,7 @@ export default function (input) {
     input.addEventListener('keypress', e => {
         if (e.key === 'Enter') {
             input.style.display = 'none';
+            input.parentNode.getElementsByTagName('p')[0].innerHTML = input.value;
             input.parentNode.getElementsByTagName('p')[0].style.display = 'block';
         }
     })
