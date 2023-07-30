aac42dd0cab1c93a7422b2f10e46d29b641c43ef
10234567Z <93607971+10234567Z@users.noreply.github.com>
2023-07-30 09:45:30 +0530

    Add area on webpage to add task and button to add it

--- src/index.js
+++ src/index.js
@@ -21,8 +21,22 @@ let addButton = document.createElement('button');
 addButton.classList.add('addProject');
 addButton.innerHTML = "Add"
 
+let taskHead = document.createElement('h1');
+taskHead.classList.add('taskHead');
+taskHead.innerHTML = 'Tasks'
+
+let addTask = document.createElement('button');
+addTask.classList.add('addProject' , 'addTask');
+addTask.innerHTML = "Add Task"
+
+let taskContainer = document.createElement('div');
+taskContainer.classList.add('taskContainer');
+
+let taskArea = document.createElement('div');
+taskArea.classList.add('taskArea');
+taskArea.append(addTask,taskContainer)
 sideBar.append(addButton,projectContainer)
-container.append(sideHead,sideBar)
+container.append(sideHead,sideBar,taskHead,taskArea)
 
 document.addEventListener('click' , e=>{
     if(e.target.classList.contains('addProject')){
