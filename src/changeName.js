fa8c29a7796ba0ddb0e3f7aa05f067a37e4f283a
10234567Z <93607971+10234567Z@users.noreply.github.com>
2023-08-13 20:41:03 +0530

    Update to not allow empty/duplicate names during editing the project/task names

--- src/changeName.js
+++ src/changeName.js
@@ -5,12 +5,15 @@ export default function (input) {
     let index = projectLibrary.findIndex(e => input.value === e.name);
     input.addEventListener('keypress', e => {
         if (e.key === 'Enter') {
-            input.style.display = 'none';
+            let validation = ProjectNameValidation(input.value);
+            if(input.value.trim() !== '' && validation){
             input.parentNode.getElementsByTagName('p')[0].innerHTML = input.value;
-            input.parentNode.getElementsByTagName('p')[0].style.display = 'block';
             projectLibrary[index].name = input.value
             currentProject.name = input.value;
             SetLibraryStorage();
+            }
+            input.style.display = 'none';
+            input.parentNode.getElementsByTagName('p')[0].style.display = 'block';
         }
     })
 }
@@ -21,11 +24,37 @@ export function taskName(input){
     let innerIndex = projectLibrary[index].todoArea.findIndex(x => x.title === input.value);
     input.addEventListener('keypress', e => {
         if (e.key === 'Enter') {
-            input.style.display = 'none';
+            let validation = TaskNameValidation(input.value);
+            if(input.value.trim() !== '' && validation){
             input.parentNode.getElementsByTagName('p')[0].innerHTML = input.value;
-            input.parentNode.getElementsByTagName('p')[0].style.display = 'block';
             projectLibrary[index].todoArea[innerIndex].title = input.value;
             SetLibraryStorage();
+            }
+            input.style.display = 'none';
+            input.parentNode.getElementsByTagName('p')[0].style.display = 'block';
         }
     })
+}
+
+function TaskNameValidation(name) {
+    let validate = true;
+    let index = projectLibrary.findIndex(e => currentProject.name === e.name)
+    for (let i = 0; i < projectLibrary[index].todoArea.length; i++) {
+        if (projectLibrary[index].todoArea[i].title.toUpperCase() === name.toUpperCase()) {
+            validate = false;
+            break;
+        }
+    }
+    return validate;
+}
+
+function ProjectNameValidation(name) {
+    let validate = true;
+    for (let i = 0; i < projectLibrary.length; i++) {
+        if (projectLibrary[i].name.toUpperCase() === name.toUpperCase()) {
+            validate = false;
+            break;
+        }
+    }
+    return validate;
 }
